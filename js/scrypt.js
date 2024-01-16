09a802ea7da86b31cec73a4a772e325be0e5a9df
MOSTAFA <58252090+mostafataheri33@users.noreply.github.com>
2024-01-16 19:52:17 +0300

    Update scrypt.js
    Js Send Email Implemented

--- js/scrypt.js
+++ js/scrypt.js
@@ -45,3 +45,60 @@ window.onscroll = () => {
     let footer = document.querySelector('footer');
     footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
+
+const form = document.querySelector('form');
+
+const FullName = document.getElementById("name");
+const EmailAddress = document.getElementById("emailaddress");
+const MobileNumber = document.getElementById("mobileNumber");
+const EmailSubject = document.getElementById("EmailText");
+const EmailText = document.getElementById("TextArea");
+
+function sendEmail() {
+    const EmailContent = "Full Name :" + FullName.value +
+        "\n\EmailAddress: " + EmailAddress.value +
+        "\n\Mobile Number: " + MobileNumber.value +
+        "\n\Email Subject: " + EmailSubject.value +
+        "\n Email Text: " + EmailText.value;
+    Email.send({
+        SecureToken: "6a96a234-a067-4a80-87fa-7241037abf87",
+        To: 'mostafataheribusiness@gmail.com',
+        From: "mostafataheribusiness@gmail.com",
+        Subject: EmailSubject.value,
+        Body: EmailContent
+    }).then(
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "Sending Email!",
+                    text: "Email Has Been Sent",
+                    icon: "success"
+                });
+            }
+        }
+    );
+}
+
+function CheckInputs() {
+    const items = document.querySelectorAll(".item")
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+        item.addEventListener("keyUp", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            } else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        });
+    }
+}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    CheckInputs();
+    sendEmail();
+});
